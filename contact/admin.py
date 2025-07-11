3d9923e3ad38f87bdb14aff772573dc9ccecf378
Anderson vidal <andersonvidal@gmail.com>
2025-07-10 22:53:48 -0300

    alteracao local_settings

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from contact import models
 
-# Register your models here.
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'first_name', 'last_name', 'phone', 'email',
+    ordering =  '-id',
+    #list_filter = 'created_date',
+    search_fields = 'first_name', 'last_name','email',
+    list_per_page = 10
+    list_max_show_all = 200
+    list_editable = 'phone', 'email',
+    list_display_links = 'first_name', 'last_name',
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering =  '-id',
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# id (primary key - automatico)
+# first_name (string), last_name (string), phone (string)
+# email (email), created_date (date), description (text)
+# category (foreign key), show (boolean), picture (image)
+
+# Depois 
+# owner (foreign key)
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories' # desnecessario qdo plural for so o "s"
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+
+
+class Contact(models.Model):
+    class Meta:
+        verbose_name = 'Contact'
+        verbose_name_plural = 'Contacts' # desnecessario qdo plural for so o "s"
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    Category = models.ForeignKey(
+        Category, 
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
+    owner = models.ForeignKey(
+        User, 
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
